1d902d73325fea18762959e606d29c41c2ebb9d2
exeynod <exeynod@gmail.com>
2020-07-23 16:44:56 +0300

    anonymous user fix x2

--- main.py
+++ main.py
@@ -3,16 +3,21 @@ from dialogues.dialogue_factory import Creator
 
 
 def handler(event, context):
-    cross_state = event['state'].get('user', {}).get('value')
+    if event['state'].get('user'):
+        cross_state = event['state']['user'].get('value')
+        state_key = 'user_state_update'
+    else:
+        cross_state = event['state']['session'].get('value')
+        state_key = 'session_state'
     user = User(cross_state)
     if event['session']['new'] and 'auth' not in user.state:
         user.state = 'greet'
     dialogue = Creator.create(user)
     result = dialogue(event['request'], user)
-    return form_response(event['version'], event['session'], result, user)
+    return form_response(event['version'], event['session'], result, user, state_key)
 
 
-def form_response(version, session, dialogue, user):
+def form_response(version, session, dialogue, user, state_key):
     if dialogue.tts == 'Default':
         dialogue.tts = dialogue.text
     res = {
@@ -22,6 +27,6 @@ def form_response(version, session, dialogue, user):
             'text': dialogue.text,
             'end_session': 'false'
         },
-        "user_state_update": {"value": user.build()}
+        state_key: {"value": user.build()}
     }
     return res

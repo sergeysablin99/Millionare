08c55b6f2a8b0c80f38324d676771b001ce6223b
exeynod <exeynod@gmail.com>
2020-07-23 16:46:56 +0300

    greetings in menu added

--- dialogues/main_menu.py
+++ dialogues/main_menu.py
@@ -3,6 +3,6 @@ from dialogues.base_dialogue import Dialogue
 
 class MainMenu(Dialogue):
 
-    def __init__(self, *args, **kwargs):
-        self.text = 'Привет. Ты в меню. Больше я ничего не умею'
+    def __init__(self, request, user):
+        self.text = f'Приветствую вас, {user.name}. Вы в меню. Больше я ничего не умею'
         self.tts = self.text

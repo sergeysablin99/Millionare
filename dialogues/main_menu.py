c446317ae3105a170b3124d536aef3ba8b60a996
Sablin Sergey <serewkasablin@outlook.com>
2020-07-29 01:27:36 +0400

    Main menu updated

--- dialogues/main_menu.py
+++ dialogues/main_menu.py
@@ -4,8 +4,8 @@ from dialogues.base_dialogue import Dialogue
 class MainMenu(Dialogue):
 
     def __init__(self, request, user):
-            self.text =  'Текст главного меню',
-            self.tts =  self.text,
+            self.text = 'Текст главного меню'
+            self.tts =  self.text
             self.card = {
                 "type": "ItemsList",
                 "header": {
@@ -41,7 +41,7 @@ class MainMenu(Dialogue):
                         "payload": {}
                     }
                 }
-            },
+            }
             self.buttons = [
                 {
                     "title": "Надпись на кнопке",

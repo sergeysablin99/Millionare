6037773fa71fcb152e768ad548da6d04e0dec88b
Sablin Sergey <serewkasablin@outlook.com>
2020-07-29 01:20:40 +0400

    Main menu updated

--- main.py
+++ main.py
@@ -20,59 +20,17 @@ def handler(event, context):
 def form_response(version, session, dialogue, user, state_key):
     if dialogue.tts == 'Default':
         dialogue.tts = dialogue.text
+    if dialogue.card:
+        response = dialogue.answer
+    else:
+        response = {
+            'text': dialogue.text,
+            'end_session': 'false'
+        }
     res = {
         'version': version,
         'session': session,
-        'end_session': 'false',
-        "response": {
-            "text": dialogue.text,
-            "tts": dialogue.text,
-            "card": {
-                "type": "ItemsList",
-                "header": {
-                    "text": "Моя прекрасная галерея",
-                },
-                "items": [
-                    {
-                        "image_id": "1030494/48a8d971e1001286fdcc",
-                        "title": "Ты не пройдешь!",
-                        "description": "Точно не пройдешь",
-                        "button": {
-                            "text": "Попробовать пройти",
-                            "url": "http://google.com",
-                            "payload": {}
-                        }
-                    },
-                    {
-                        "image_id": "1030494/274ba5413144f7607711",
-                        "title": "Техника соблазнения",
-                        "description": "Научись соблазну вместе с Хатаке Какаши!",
-                        "button": {
-                            "text": "Попробовать бесплатно",
-                            "url": "http://google.com",
-                            "payload": {}
-                        }
-                    }
-                ],
-                "footer": {
-                    "text": "Текст блока под изображением.",
-                    "button": {
-                        "text": "Надпись на кнопке",
-                        "url": "https://example.com/",
-                        "payload": {}
-                    }
-                }
-            },
-            "buttons": [
-                {
-                    "title": "Надпись на кнопке",
-                    "payload": {},
-                    "url": "https://google.com/",
-                    "hide": 'true'
-                }
-            ],
-            "end_session": 'false'
-        },
+        'response': response,
         state_key: {"value": user.build()}
     }
     return res

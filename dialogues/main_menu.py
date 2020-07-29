61475e428ce09ec5ce64c7780e9811d43243bd5c
Sablin Sergey <serewkasablin@outlook.com>
2020-07-29 18:36:35 +0400

    Menu updated

--- dialogues/main_menu.py
+++ dialogues/main_menu.py
@@ -5,7 +5,7 @@ from config import give_buttons, make_menu, get_file, menus
 class MainMenu(Dialogue):
 
     def __init__(self, request, user):
-        self.text = 'Текст главного меню'
+        self.text = 'Вы в главном меню. Желаете сыграть, посмотреть достижения или ознакомиться с рейтингом игроков?'
         self.tts = self.text
         self.card = get_file(menus, 'main_menu')
         self.card = make_menu(self.card['title'], self.card['content'])

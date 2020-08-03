5b86b7f78430c474a3aa01b07602b0dbc3d387f9
exeynod <exeynod@gmail.com>
2020-08-03 13:48:48 +0300

    answer checking fixed

--- dialogues/game.py
+++ dialogues/game.py
@@ -5,7 +5,7 @@ from dialogues.base_dialogue import Dialogue
 class Game(Dialogue):
 
     def check_answer(self, tokens, level, question):
-        return len(set(get_file(questions_path[self.define_level(level)], 'questions')[question]) & tokens) > 0
+        return len(set(get_file(questions_path[self.define_level(level)], 'questions')[question]['right']) & tokens) > 0
 
     def __init__(self, request, user):
         tokens = set(request['nlu']['tokens'])
